6e613af24c4717b1b8cbffd0b07686acc24bb2b8
xlsanchez <sandovalxavier0812@gmail.com>
2025-10-31 17:46:40 +0000

    advanced settings

--- 2_working_with_strings.py
+++ 2_working_with_strings.py
@@ -96,11 +96,13 @@ print(len(decleration_of_independence))
 # # String Methods
 # # ----------------------------------------
 
-# sentence = "Python is fun to learn"
+sentence = "Python is fun to learn"
 
 # # .split(): Splits the string into a list of words
-# words = sentence.split()
-# print("Split result:", words)
+words = sentence.split()
+print("Split result:", words)
+words2 = sentence.join("")
+print(words2)
 
 # # .format(): Allows inserting values into strings using {}
 # name = "Marvin"
