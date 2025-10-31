bafae465af4fd08ed5bc1190b19723a802ca1d1d
xlsanchez <sandovalxavier0812@gmail.com>
2025-10-31 17:40:56 +0000

    bell ringer

--- week_11_Day2.py
+++ week_11_Day2.py
@@ -9,24 +9,25 @@ print("You’ll review variables, strings, numbers, and print formatting.\n")
 
 # Step 1: Create Variables
 # TODO: Replace the values below with your own info
-first_name = "Marvin"
-age = 30
+first_name = "Xavier"
+age = 16
 favorite_color = "Blue"
-favorite_number = 7
+favorite_number = 12
 
 #  Step 2: Practice String Operations
 # 1. Print your name in uppercase
-
+print(first_name.upper())
 
 # 2. Print how many letters are in your name
-
+print(len(first_name))
 
 # 3. Combine your name and favorite color into one message
-
+print("My first name is" , (first_name), " and my favorite color is " , (favorite_color))
 
 #  Step 3: Math Practice
 # Use arithmetic operators with your favorite number
-
+x = 5
+print(f"{x + favorite_number}")
 
 #  Step 4: User Input Practice
 # Ask the user two questions and combine answers
